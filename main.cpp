1b2814a4bc25f66e7bba335ec44b48a1b62f4328
yevka <yevka.vladyslav@gmail.com>
2018-05-22 22:37:00 +0300

    Adds an exception handler in main.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,22 @@
 #include <iostream>
+#include <exception>
 
 #include "Engine.h"
 
 int main(int argc, char** argv) {
+  try {
+    Engine taipan;
+    taipan.EngineInfo();
+    taipan.Init();
+    taipan.Loop(argc, argv);
 
-  Engine taipan;
-  taipan.EngineInfo();
-  taipan.Init();
-  taipan.Loop(argc, argv);
+  } catch(std::exception& e) {
+    std::cerr << "Error: " << e.what() << std::endl;
+    return 1;
+  } catch(...) {
+    std::cerr << "Unknown error" << std::endl;
+    return 2;
+  }
 
   return 0;
 }
